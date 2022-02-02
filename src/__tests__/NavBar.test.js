6e14cc73122f30f70eefa4af1f9cf718919bb567
Will Lovell <willard.lovell@icloud.com>
2022-02-01 22:20:36 -0700

    Completed Assignment

--- src/__tests__/NavBar.test.js
+++ src/__tests__/NavBar.test.js
@@ -3,9 +3,9 @@ import { render } from "@testing-library/react";
 import NavBar from "../components/NavBar";
 
 test("it is exported as a default export", () => {
-  try {
-    expect(() => render(<NavBar />)).not.toThrow();
-  } catch (e) {
-    throw new Error("Make sure to export this component!");
-  }
+    try {
+        expect(() => render( < NavBar / > )).not.toThrow();
+    } catch (e) {
+        throw new Error("Make sure to export this component!");
+    }
 });
